3d1b7cc06bcd20b9f3411cdd8205048d817fa372
Paweł Wojdas <paw.wojdas@gmail.com>
2020-08-14 21:29:56 +1000

    Updates

---
+++ precache-manifest.e9e57a0a43ab9f14ebabffdd0b746b57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6749ff784488a1418aada83cb878ee59",
+    "url": "/pawel-wojdas-website/index.html"
+  },
+  {
+    "revision": "0e9c92ed4218b8dcb285",
+    "url": "/pawel-wojdas-website/static/css/main.bed84e31.chunk.css"
+  },
+  {
+    "revision": "dc62953cbf5ba23ab74e",
+    "url": "/pawel-wojdas-website/static/js/2.cee37be1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pawel-wojdas-website/static/js/2.cee37be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9c92ed4218b8dcb285",
+    "url": "/pawel-wojdas-website/static/js/main.05aa6def.chunk.js"
+  },
+  {
+    "revision": "ebb7b3ec37d34b05dd34",
+    "url": "/pawel-wojdas-website/static/js/runtime-main.cb18b459.js"
+  },
+  {
+    "revision": "a972cdf3d7eccd3e95cfd41ab8695d77",
+    "url": "/pawel-wojdas-website/static/media/logo.a972cdf3.svg"
+  }
+]);
